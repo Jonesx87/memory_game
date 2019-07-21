9fb214d8f7d7585969367a2689ac71ed9029268a
Shaun Jones <shaunjones@Shauns-MacBook-Air.local>
2019-07-21 00:19:29 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
+// !! or && and ! not
+
 console.log("Up and Running!")
 
-var cardOne = "Queen";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "King";
+
+// var cardOne = "Queen";
+// var cardTwo = "King";
+// var cardThree = "Queen";
+// var cardFour = "King";
+
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+var cards = ['Queen', 'Queen', 'King', 'King'];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+    cardsInPlay.push('cardOne');
+
+var cardTwo = cards[2];
+    cardsInPlay.push('cardTwo');
+    if (cardsInPlay.length) === 2; {
+        alert("You found a Match") {
+        }
+    if else (cardsInPlay[0] === cardsInPlay[2]); {
+        alert("Sorry Try Again!");
+    }
