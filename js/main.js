e6c7a2c9588252a3c1da1d8218e890b25d13b68c
Shaun Jones <shaunjones@Shauns-MacBook-Air.local>
2019-07-21 10:48:00 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Since pre work not deleting code and keeping notes
 // !! or && and ! not
 
 console.log("Up and Running!")
@@ -9,22 +10,62 @@ console.log("Up and Running!")
 // var cardFour = "King";
 
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardTwo);
 
 var cards = ['Queen', 'Queen', 'King', 'King'];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+      } else {
+        console.log("Sorry, try again.");
+      }
+}
+
+flipCard(checkForMatch(1));
+// remember to study mistakes
+//var cardOne = cards[0];
+
+ //   cardsInPlay.push('cardOne');
+
+//var cardTwo = cards[2];
+//    cardsInPlay.push('cardTwo');
+    
+ //   if (cardsInPlay.length) === 2; {
+ //       alert("You found a Match") {
+ //       }
+//    if else (cardsInPlay[0] === cardsInPlay[2]); {
+//        alert("Sorry Try Again!");
+//    }
+//if (cardsInPlay.length[] <=== 2) {
+//    alert("You found a Match"); {
+//    } else {
+//        alert("Sorry Try Again!");
+//    }
+//};
+//
+//
+function flipCard(cardID) {
+    console.log("User flipped" + cards[cardID]); 
+
+    var cardsInPlay = [];
+
+    var cardOne = cards[0];
 
     cardsInPlay.push('cardOne');
 
-var cardTwo = cards[2];
-    cardsInPlay.push('cardTwo');
-    if (cardsInPlay.length) === 2; {
-        alert("You found a Match") {
-        }
-    if else (cardsInPlay[0] === cardsInPlay[2]); {
-        alert("Sorry Try Again!");
-    }
+    var cardTwo = cards[2];
+    
+    if (cardsInPlay.length[] % 2) {
+        alert("You found a Match")
+    } else {
+        alert("Sorry try Again!")
+    };
+    
+    cardsInPlay.push(cards[cardID]);
+
+    flipCard(0);
+    flipCard(2);
